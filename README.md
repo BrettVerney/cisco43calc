afa4683d1f5a0ba6260da26f37fb954266d06a38
Brett Verney <44787975+wifiwizardofoz@users.noreply.github.com>
2023-06-30 11:32:34 +1000

    Complete tidy up and rewrite of code

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A python DHCP Option 43 Generator for Cisco Lightweight APs
 **cisco43calc.py**
 
 **Author:** Brett Verney</br>
-**Version:** v1.0 | 30-09-2020
+**Version:** v2.0 | 30-6-2023
 
 This script converts a list of user supplied IP addresses to a TLV hexadecimal string required for DHCP Option 43 as used by Cisco Lightweight Access Point (LWAPPs) for discovering Cisco Wireless LAN Controllers (WLCs).
 
--- cisco43calc.py
+++ cisco43calc.py
@@ -1,90 +1,68 @@
-# Script by Brett Verney | @wifiwizardofoz
-# Version: v1.0 | 30-09-2020
-
 import sys
 import ipaddress
 from ipaddress import AddressValueError
 import socket
 
-# User executes script with IPs specified as arguments at CLI
-def arg_mode():
+# Function to validate and convert IP addresses to hexadecimal.
+def ip_to_hex(wlc_ip):
     try:
-        ip_args = (sys.argv[1::])
-        iplist = []
-        for wlc_ip in ip_args:
-            # Validate IP addresses and convert to hex
-            wlc_ip = ipaddress.IPv4Address(wlc_ip)
-            wlc_ip = str(wlc_ip)
-            wlc_ip = socket.inet_aton(wlc_ip).hex()
-            # Ignore duplicate entries, convert to list and join on a single line with no spacing
-            if wlc_ip not in iplist:
-                iplist.append(wlc_ip)
-            else:
-                continue
-            hexlist = (''.join(iplist))
-        # Convert IP count to hex/base 16, remove leading '0xf'. Print =< 2 digits or add leading '0' as required by Cisco
-        hexcount = int(hex(len(iplist)*4), 16)
-        hexcount = '{:02X}'.format(hexcount)
-        # Combine and print output to screen for user
-        print ('f1' + hexcount + hexlist)
+        wlc_ip = ipaddress.IPv4Address(wlc_ip)
+        wlc_ip = str(wlc_ip)
+        wlc_ip = socket.inet_aton(wlc_ip).hex()
+        return wlc_ip
     except AddressValueError:
-        sys.exit('Error: One or more specified arguments are not Valid IPv4 Addresses')
+        return None
 
-# User runs script without any arguments specified at CLI
-def interactive_mode():
-    try:
-        count = int(0)
-        user_input = int(input('\n' + 'Number of WLCs in network: '))
-        assert 0 < user_input < 17
-    except AssertionError:
-        print ('\n' + 'Please enter a number between 1 and 16, or press CTRL+C to exit.')
-        main()
-    except KeyboardInterrupt:
-        sys.exit(0)
-    except:
-        print ('\n' + 'Not a valid number. Try again or press CTRL+C to exit.')
-        main()
-    else:
-        iplist = []
-        while count < user_input:
-            count += 1
-            try:
-                # Present user with required number of 'input' prompt. Validate IP addresses and convert to hex
-                wlc_ip = ipaddress.IPv4Address(input('WLC #' + str(count) + ' IP Address: '))
-                wlc_ip = str(wlc_ip)
-                wlc_ip = socket.inet_aton(wlc_ip).hex()
-                # Don't allow duplicate entries and append to list
-                if wlc_ip not in iplist:
-                    iplist.append(wlc_ip)
-                else:
-                    count -= 1
-                    print ('\n' + 'IP Address already entered. Try again or press CTRL+C to exit.')
-            except KeyboardInterrupt:
-                sys.exit(0)
-            except:
-                count -= 1
-                print ('\n' + 'Not a valid IP Address. Try again or press CTRL+C to exit.')
-            else:
-                pass
-        # Join hex formatted IP addresses together in single line, with no spacing
-        hexlist = (''.join(iplist))
-       # Convert IP count to hex/base 16, remove leading '0xf'. Print =< 2 digits or add leading '0' as required by Cisco
-        hexcount = int(hex(len(iplist)*4), 16)
-        hexcount = '{:02X}'.format(hexcount)
-        # Combine and print output to screen for user
-        print ('\n' + 'Your DHCP Option 43 value is: ' + 'f1' + hexcount + hexlist)
+# Function to print the list of IP addresses in the required format.
+def print_hex_ips(iplist):
+    hexlist = ''.join(iplist)
+    hexcount = '{:02X}'.format(int(hex(len(iplist)*4), 16))
+    print ('\n' + 'Your DHCP Option 43 value is: ' + 'f1' + hexcount + hexlist)
 
+# Function to handle case when script is executed with command line arguments.
+def arg_mode(ip_args):
+    iplist = []
+    for wlc_ip in ip_args:
+        hex_ip = ip_to_hex(wlc_ip)
+        if hex_ip is None:
+            print('Error: One or more specified arguments are not Valid IPv4 Addresses')
+            return
+        if hex_ip not in iplist:
+            iplist.append(hex_ip)
+    print_hex_ips(iplist)
+
+# Function to handle case when script is executed without command line arguments.
+def interactive_mode(user_input):
+    count = 0
+    iplist = []
+    while count < user_input:
+        count += 1
+        wlc_ip = input('WLC #' + str(count) + ' IP Address: ')
+        hex_ip = ip_to_hex(wlc_ip)
+        if hex_ip is None:
+            print ('Not a valid IP Address. Try again.')
+            count -= 1
+        elif hex_ip in iplist:
+            print ('IP Address already entered. Try again.')
+            count -= 1
+        else:
+            iplist.append(hex_ip)
+    print_hex_ips(iplist)
+
+# Main function to handle the overall logic.
 def main():
     if len(sys.argv) > 1:
-        arg_mode()
+        arg_mode(sys.argv[1::])
     else:
-        interactive_mode()
-        
-# Start Here
+        try:
+            user_input = int(input('\n' + 'Number of WLCs in network: '))
+            if 0 < user_input < 17:
+                interactive_mode(user_input)
+            else:
+                print ('Please enter a number between 1 and 16.')
+        except ValueError:
+            print ('Not a valid number. Try again.')
 
-# Script is executed as main program
+# Execution starts here.
 if __name__ == "__main__":
     main()
-# Script has been imported from within another module
-else:
-    arg_mode()
